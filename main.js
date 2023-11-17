3a1c96a1296055a81bc8bcdd8139c3d1bcc58d0f
AMR <amremail@gmail.com>
2023-11-17 14:08:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// let delete= document.getElementById('delete');
+// delete.onclick = function () {
+//     delete.parent.style.displaye=None;
+// }
+console.log(screen.availWidth);
+console.log(screen.avaikheight);
